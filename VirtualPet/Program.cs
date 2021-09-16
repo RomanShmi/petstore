9703bd6c3306836b6c013e65e4b7561dd7e801ab
CrysD5 <Matrim.ivy@gmail.com>
2021-09-16 17:46:06 -0400

    Committing program.cs after working on timer during code crammerz .

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 
+using System.Threading;
+
+
+
 namespace VirtualPet
+
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello! Welcome to Virtual Pets");
-        }
-    }
+
+    class Program
+
+    {
+
+        // we need a pet 'property' variable
+
+        // public Pet myPet ...
+
+        static void Main(string[] args)
+
+        {
+
+        //  Console.WriteLine("Hello! Welcome to Virtual Pets");
+
+            // ask the user about their pet's info
+
+            // reassign myPet's properties to be what the user provided
+
+            // myPet.name = Console.ReadLine();
+
+            Timer _timer = new Timer(Tick, null, 0, 15000);
+
+            while (true)
+
+            {
+
+                // Ask the user what they want to do
+
+                // etc.
+
+            }
+
+        }
+
+
+
+        public static void Tick(Object o)
+
+        {
+
+            // put methods/updates that change the pet in some way
+
+            Console.WriteLine("Tick");
+
+        }
+
+    }
+
 }
